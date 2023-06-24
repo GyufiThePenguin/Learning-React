85ca67c4a5ecf220d8052b99c0e884c2ec272fec
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-06-24 18:52:51 +0200

    Making date readable

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -8,9 +8,17 @@ function ExpenseItem(props) {
     // You caan access an object prop like:
     // <h2>{props.expense.title}</h2>
 
+    const month = props.date.toLocaleString('hu-HU',{month:"long"});
+    const day = props.date.toLocaleString('hu-HU',{day:"2-digit"});
+    const year = props.date.getFullYear();
+
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
