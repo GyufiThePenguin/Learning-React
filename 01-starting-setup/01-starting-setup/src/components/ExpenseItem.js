36ad703f984c9589c0c1bb6dd8242814d3e5b6b3
GyufiThePenguin <tothzsombor200101@gmail.com>
2023-06-28 13:15:31 +0200

    Using sub component in a component, tunneling props

--- 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 
 // You can use JS Object destructuring for props:
@@ -8,17 +9,13 @@ function ExpenseItem(props) {
     // You caan access an object prop like:
     // <h2>{props.expense.title}</h2>
 
-    const month = props.date.toLocaleString('hu-HU',{month:"long"});
-    const day = props.date.toLocaleString('hu-HU',{day:"2-digit"});
-    const year = props.date.getFullYear();
+    
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+
+            <ExpenseDate date={props.date}/>
+
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
